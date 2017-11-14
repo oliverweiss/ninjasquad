3d27f5f4f514ca835b40c841c793fbe8e13500f2
Olivier Blanc <olivier.blanc@gmail.com>
2017-11-14 14:29:08 +0100

    Exercise 0

--- ponyracer/karma.conf.js
+++ ponyracer/karma.conf.js
@@ -4,25 +4,32 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
+    browserNoActivityTimeout: 30000,
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-firefox-launcher'),
+      require('karma-json-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: [ 'html', 'json-summary'],
       fixWebpackSourcePaths: true
+    },
+    jsonReporter: {
+      stdout: false,
+      outputFile: 'results/karma-results.json'
     },
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'json', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
