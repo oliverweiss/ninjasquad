e9f711c4256bf9dc0f023169b8bf01ea5c8caf7d
Olivier Blanc <olivier.blanc@gmail.com>
2017-11-14 16:54:14 +0100

    List of races - done

---
+++ ponyracer/src/app/races/races.component.ts
@@ -0,0 +1,18 @@
+import { RaceModel } from '../models/race.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pr-races',
+  templateUrl: './races.component.html',
+  styleUrls: ['./races.component.css']
+})
+export class RacesComponent implements OnInit {
+  races: Array<RaceModel>;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.races = [{ name: 'Lyon' }, { name: 'London' }];
+  }
+
+}
