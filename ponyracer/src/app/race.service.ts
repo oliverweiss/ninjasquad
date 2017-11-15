6b0c8b2c339f8ad9dbb581bf358a21d5f46431ad
Olivier Blanc <olivier.blanc@gmail.com>
2017-11-15 13:45:39 +0100

    Observable - done

--- ponyracer/src/app/race.service.ts
+++ ponyracer/src/app/race.service.ts
@@ -1,3 +1,6 @@
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
 import { RaceModel } from './models/race.model';
 import { Injectable } from '@angular/core';
 
@@ -6,13 +9,13 @@ export class RaceService {
 
   constructor() { }
 
-  list(): Array<RaceModel> {
-    return [
+  list(): Observable<Array<RaceModel>> {
+    return Observable.of([
       {name: 'Lyon'},
       {name: 'Los Angeles'},
       {name: 'Sydney'},
       {name: 'Tokyo'},
       {name: 'Casablanca'}
-    ];
+    ]);
   }
 }
