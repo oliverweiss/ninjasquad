ea3f0c2ea6b1c338054c327fb4474e59a2f9740f
Olivier Blanc <olivier.blanc@gmail.com>
2017-11-15 11:04:50 +0100

    Pipes - done

--- ponyracer/src/app/race.service.ts
+++ ponyracer/src/app/race.service.ts
@@ -7,6 +7,12 @@ export class RaceService {
   constructor() { }
 
   list(): Array<RaceModel> {
-    return [{ name: 'Lyon' }, { name: 'London' }];
+    return [
+      {name: 'Lyon'},
+      {name: 'Los Angeles'},
+      {name: 'Sydney'},
+      {name: 'Tokyo'},
+      {name: 'Casablanca'}
+    ];
   }
 }
